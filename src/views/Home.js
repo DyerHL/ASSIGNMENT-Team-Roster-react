1ee9393e4086eb2ae39332a28f0d4483867647d1
Halie Dyer <Halie.L.Dyer@gmail.com>
2021-10-22 14:58:39 -0400

    added promises, form, and player cards

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Players from '../components/Players';
 
-export default function Home() {
+export default function Home({ players, setPlayers, setEditItem }) {
   return (
     <div>
-      <h1>Add A Player Form</h1>
+      <div className="mt-5">
+        {players.map((player) => (
+          <Players
+            key={player.firebaseKey}
+            player={player}
+            setPlayers={setPlayers}
+            setEditItem={setEditItem}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
+Home.propTypes = {
+  players: PropTypes.arrayOf(PropTypes.object).isRequired,
+  setPlayers: PropTypes.func.isRequired,
+  setEditItem: PropTypes.func.isRequired,
+};
